e3c4dbb7cc0b2b2e4bb26bbaba4f569981acd44e
chris820629 <chris820629@gmail.com>
2022-12-06 16:45:29 -0800

    adding car manufacturing lot to psi relationship

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,11 @@ car_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F)
 linear_car <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=car_df)
 
 summary(linear_car)
+
+##suspension coil analysis 
+sus_df <- read.csv('Suspension_Coil.csv',stringsAsFactors = F)
+
+total_summary <- sus_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary <- sus_df %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
